54801f7b088e3ac2b48f6f10bca88f1b5e770711
bhagwatvs <138701493+bhagwatvs@users.noreply.github.com>
2024-05-15 14:51:23 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+      navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+    } else {
+      navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    }
+  });
+  
